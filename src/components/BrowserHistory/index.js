6cd2369fb6b2d5939bb477b6260d0687577e62f5
Coding-Container <blgnd227@gmail.com>
2025-12-28 16:26:08 +0530

    Correct capitalization in no history message

--- src/components/BrowserHistory/index.js
+++ src/components/BrowserHistory/index.js
@@ -145,7 +145,7 @@ class BrowserHistory extends Component {
               ))}
             </ul>
           ) : (
-            <p>There is no history to show</p>
+            <p>There is no history to Show</p>
           )}
         </div>
       </div>
